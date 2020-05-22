b2a9a2e34101b4e0f85561f39563084e3ed3067b
skynoorz <calaveras6456>
2020-05-21 19:13:10 -0500

    primer commit uquest2

---
+++ src/main/java/uquest/com/bo/UquestAngularApplication.java
@@ -0,0 +1,13 @@
+package uquest.com.bo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UquestAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UquestAngularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uquest/com/bo/UquestAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package uquest.com.bo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UquestAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
