cafc6aab7e86dd015c8e06129fd6878b5e61192c
skynoorz <calaveras6456>
2020-06-22 11:22:04 -0500

    problem with formgroups and formcontrol

---
+++ angular/src/app/usuarios/login.component.ts
@@ -0,0 +1,58 @@
+import {Component, OnInit} from '@angular/core';
+import {Persona} from "../personas/persona";
+import Swal from "sweetalert2";
+import {AuthService} from "./auth.service";
+import {Router} from "@angular/router";
+import {Rol} from "../classes/rol";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  titulo: string = "Porfavor Sign In"
+  persona: Persona;
+
+  constructor(private authService: AuthService, private router: Router) {
+    this.persona = new Persona()
+  }
+
+  ngOnInit(): void {
+    if (this.authService.isAuthenticated()) {
+      this.router.navigate(['/personas'])
+      Swal.fire("Login", `${this.authService.persona.username} ya te encuentras autenticado`, "info")
+    }
+  }
+
+  login(): void {
+    console.log(this.persona);
+    if (this.persona.username == null || this.persona.password == null) {
+      Swal.fire("Error Login", "Username o password vacias!", "error");
+    }
+    //aca guardo en el session storage
+    this.authService.login(this.persona).subscribe(response => {
+      console.log(response);
+
+      this.authService.guardarPersona(response.access_token);
+      this.authService.guardarToken(response.access_token);
+
+      let usuario = this.authService.persona;
+
+      // let rols: Rol = new Rol();
+      // rols.nombre = "ROLE_ADMIN";
+      console.log("hasRole: "+this.authService.hasRole("ROLE_ADMIN"))
+      if (this.authService.hasRole("ROLE_ADMIN"))
+        this.router.navigate(['/personas'])
+      else
+        this.router.navigate(['/'])
+      Swal.fire('Login', `Hola ${usuario.username}, has iniciado sesion con exito!`, "success")
+    }, error => {
+      if (error.status == 400) {
+        Swal.fire("Error Login", "Usuario o clave incorrecta", 'error');
+      }
+    })
+  }
+
+}
---
+++ src/main/java/uquest/com/bo/models/services/IUsuarioService.java
@@ -0,0 +1,31 @@
+package uquest.com.bo.models.services;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import uquest.com.bo.models.entity.Carrera;
+import uquest.com.bo.models.entity.Instituto;
+import uquest.com.bo.models.entity.Role;
+import uquest.com.bo.models.entity.Usuario;
+
+import java.util.List;
+
+public interface IUsuarioService {
+
+    public List<Usuario> findAll();
+
+    public Page<Usuario> findAll(Pageable pageable);
+
+    public Usuario findById(Long id);
+
+    public Usuario save(Usuario usuario);
+
+    public void delete(Long id);
+
+    public List<Carrera> findAllCarreras();
+
+    public List<Instituto> findAllInstitutos();
+
+    public List<Role> findAllRoles();
+
+    Usuario findByUsername(String name);
+}
