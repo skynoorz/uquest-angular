aeb7a2b85b1770bce049850d5d85a8d3b520e236
Mauricio Enriquez <MAURICIO.ENRIQUEZ@consultant.vodafoneomnitel.it>
2020-12-16 22:07:42 +0100

    added maven plugin to serve angular on spring

--- src/main/java/uquest/com/bo/auth/ResourceServerConfig.java
+++ src/main/java/uquest/com/bo/auth/ResourceServerConfig.java
@@ -20,7 +20,9 @@ import java.util.Arrays;
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers(HttpMethod.GET,"/api/uploads/img/**", "/images/no_user.png", "/api/carreras", "/api/carreras/institutos/**", "/api/usuarios/encuestas/**", "/api/upr/**", "/api/usuarios/emailexist/**", "/api/categorias/**", "/api/encuestas/public/**", "/api/respuestas/encuesta/**").permitAll()
+        http.authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .antMatchers(HttpMethod.GET,"/api/uploads/img/**", "/images/no_user.png", "/api/carreras", "/api/carreras/institutos/**", "/api/usuarios/encuestas/**", "/api/upr/**", "/api/usuarios/emailexist/**", "/api/categorias/**", "/api/encuestas/public/**", "/api/respuestas/encuesta/**").permitAll()
                 .antMatchers(HttpMethod.POST, "/api/usuarios",  "/api/encuestas/**").permitAll()
                 .antMatchers(HttpMethod.GET, "/api/encuestas/**","/api/respuesta/usuarios/pregunta/public/**").permitAll()
                 .antMatchers(HttpMethod.GET, "/api/usuarios/userexist/**").permitAll()
