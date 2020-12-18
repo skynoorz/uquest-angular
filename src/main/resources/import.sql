9c30db900e94ee988063d835461158cc49d4a9b8
skynoorz <calaveras6456>
2020-12-18 16:40:38 -0400

    removed all manual insert ids v2mb

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -31,8 +31,8 @@ INSERT INTO preguntas_opciones (pregunta_id, opcion_id) VALUES (2,1);
 INSERT INTO preguntas_opciones (pregunta_id, opcion_id) VALUES (2,2);
 
 
-INSERT INTO roles (nombre) VALUES (1, 'ROLE_USER');
-INSERT INTO roles (nombre) VALUES (2, 'ROLE_ADMIN');
+INSERT INTO roles (nombre) VALUES ('ROLE_USER');
+INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
 
 INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (1,1);
 INSERT INTO usuarios_roles (usuario_id, role_id) VALUES (2,2);
