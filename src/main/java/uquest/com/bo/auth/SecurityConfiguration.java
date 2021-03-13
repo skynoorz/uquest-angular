c3350e98eea355027d3e5268262b45a9c38957fe
skynoorz <calaveras6456>
2021-03-13 01:03:21 -0400

    fixed query for export csv and deleting encuesta (npm run build) trying to fix error 3

--- src/main/java/uquest/com/bo/auth/SecurityConfiguration.java
+++ src/main/java/uquest/com/bo/auth/SecurityConfiguration.java
@@ -79,7 +79,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 "/api/respuesta/usuarios/pregunta/public/**",
                 "/api/carreras/institutos/**",
                 "/api/usuarios/emailexist/**",
-                "/api/usuarios/ciexist/**").permitAll()
+                "/api/usuarios/ciexist/**","/resources/**").permitAll()
 //                GET control
                 .antMatchers(HttpMethod.GET, "/api/usuarios/page/**").hasAuthority("ROLE_ADMIN")
                 .antMatchers(HttpMethod.GET, "/api/usuarios/encuestas/user/**","/api/encuestas/finalizar/**").hasAuthority("ROLE_USER")
@@ -95,9 +95,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .apply(securityConfigurerAdapter());
-        http.requiresChannel()
-                .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
-                .requiresSecure();
         // @formatter:on
     }
 
