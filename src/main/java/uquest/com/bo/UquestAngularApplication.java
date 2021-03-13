1b41bc715094a677d49c09ea9b2448f7a75aa0f0
skynoorz <calaveras6456>
2021-03-13 00:20:02 -0400

    fixed query for export csv and deleting encuesta (npm run build)

--- src/main/java/uquest/com/bo/UquestAngularApplication.java
+++ src/main/java/uquest/com/bo/UquestAngularApplication.java
@@ -1,27 +1,13 @@
 package uquest.com.bo;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class UquestAngularApplication implements CommandLineRunner {
+public class UquestAngularApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UquestAngularApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-//		String password = "ronaldg";
-//		for (int i = 0; i < 4; i++) {
-//			String passwordBcrypt = passwordEncoder.encode(password);
-//			System.out.println(passwordBcrypt);
-////			String uid = UUID.randomUUID().toString();
-////			System.out.println(uid);
-//		}
-
-	}
 }
